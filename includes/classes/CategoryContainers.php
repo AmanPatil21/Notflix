fe02c7f41399e4b41defefb4b7e0f37446d1b7d9
Vivek Bhand <68386459+VivekBhand@users.noreply.github.com>
2022-04-07 13:41:28 +0530

    Add files via upload

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -1,35 +1,60 @@
-<?php 
-require_once("includes/config.php");
-class CategoryContainers{
+<?php
+class CategoryContainers {
 
-    private $con , $userName;
-    public function __construct($con,$userName) {
-        $this->con = $con ;
-        $this->userName = $userName ;
+    private $con, $userName;
+
+    public function __construct($con, $userName) {
+        $this->con = $con;
+        $this->userName = $userName;
     }
-
 
     public function showAllCategories() {
-        $query = $this->con->prepare("SELECT * FROM categories") ;
-        $query->execute() ;
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
 
-        $html  = "<div class='previewCategories'>" ;
+        $html = "<div class='previewCategories'>";
 
         while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            $html .= $this->getCategoryHtml($row ,null , true , true) ;
+            $html .= $this->getCategoryHtml($row, null, true, true);
         }
 
-        return $html . "</div>" ;
+        return $html . "</div>";
     }
 
+    private function getCategoryHtml($sqlData, $title, $tvShows, $movies) {
+        $categoryId = $sqlData["id"];
+        $title = $title == null ? $sqlData["name"] : $title;
 
-    private function getCategoryHtml($sqlData , $title , $tvShows  , $movies) {
-        $categoryId = $sqlData["id"] ;
-        $title  =  $title == null ? $sqlData["name"] :$title ;
+        if($tvShows && $movies) {
+            $entities = EntityProvider::getEntities($this->con, $categoryId, 30);
+        }
+        else if($tvShows) {
+            // Get tv show entities
+        }
+        else {
+            // Get movie entities
+        }
 
-        return $title . "<br>" ;
+        if(sizeof($entities) == 0) {
+            return;
+        }
+
+        $entitiesHtml = "";
+        $previewProvider = new PreviewProvider($this->con, $this->userName);
+        foreach($entities as $entity) {
+            $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
+        }
+
+        return "<div class='category'>
+                    <a href='category.php?id=$categoryId'>
+                        <h3>$title</h3>
+                    </a>
+
+                    <div class='entities'>
+                        $entitiesHtml
+                    </div>
+                </div>";
     }
 
 }
-
 ?>
--- includes/header.php
+++ includes/header.php
@@ -1,29 +1,29 @@
-<?php 
+<?php
+require_once("includes/config.php");
+require_once("includes/classes/PreviewProvider.php");
+require_once("includes/classes/CategoryContainers.php");
+require_once("includes/classes/Entity.php");
+require_once("includes/classes/EntityProvider.php");
+require_once("includes/classes/ErrorMessage.php");
+require_once("includes/classes/SeasonProvider.php");
+require_once("includes/classes/Season.php");
+require_once("includes/classes/Video.php");
 
-    require_once("includes/config.php");
-    require_once("includes/classes/PreviewProvider.php");
-    require_once("includes/classes/CategoryContainers.php");
-    require_once("includes/classes/Entity.php");
-    if(!isset($_SESSION["userLoggedIn"])) {
-        header("Location: register.php");
-    }
+if(!isset($_SESSION["userLoggedIn"])) {
+    header("Location: register.php");
+}
 
-    $userLoggedIn = $_SESSION["userLoggedIn"];
-
+$userLoggedIn = $_SESSION["userLoggedIn"];
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
-        <link rel = "stylesheet" type="text/css" href = "assests\style\style.css" />
-       
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-        <script src="https://kit.fontawesome.com/ff0d75d102.js" crossorigin="anonymous"></script>
-        <script src="assests/js/script.js"></script>
-</head >
-<body>
-    <div class="wrapper">
+        <title>Welcome to NotFlix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
 
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"></script>
+        <script src="assets/js/script.js"></script>
+    </head>
+    <body>
+        <div class='wrapper'>
