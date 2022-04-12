8fe22e237081c3facffe1660727c03884154ed3b
Vivek Bhand <68386459+VivekBhand@users.noreply.github.com>
2022-04-12 19:45:36 +0530

    Final Project Version : assets folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,18 +16,74 @@ function goBack() {
 }
 
 function startHideTimer() {
-    var timeOut = null;
-
+    var timeout = null;
+    
     $(document).on("mousemove", function() {
-        clearTimeout(timeOut);
+        clearTimeout(timeout);
         $(".watchNav").fadeIn();
 
-        timeOut = setTimeout(function() {
+        timeout = setTimeout(function() {
             $(".watchNav").fadeOut();
-        },2000);
+        }, 2000);
     })
 }
 
-function initVideo() {
+function initVideo(videoId, userName) {
     startHideTimer();
+    setStartTime(videoId, userName);
+    updateProgressTimer(videoId, userName);
+}
+
+function updateProgressTimer(videoId, userName) {
+    addDuration(videoId, userName);
+
+    var timer;
+
+    $("video").on("playing", function(event) {
+        window.clearInterval(timer);
+        timer = window.setInterval(function() {
+            updateProgress(videoId, userName, event.target.currentTime);
+        }, 3000);
+    })
+    .on("ended", function() {
+        setFinished(videoId, userName);
+        window.clearInterval(timer);
+    })
+}
+
+function addDuration(videoId, userName) {
+    $.post("ajax/addDuration.php", { videoId: videoId, userName: userName }, function(data) {
+        if(data !== null && data !== "") {
+            alert(data);
+        }
+    })
+}
+
+function updateProgress(videoId, userName, progress) {
+    $.post("ajax/updateDuration.php", { videoId: videoId, userName: userName, progress: progress}, function(data) {
+        if(data !== null && data !== "") {
+            alert(data);
+        }
+    })
+}
+
+function setFinished(videoId, userName) {
+    $.post("ajax/setFinished.php", { videoId: videoId, userName: userName}, function(data) {
+        if(data !== null && data !== "") {
+            alert(data);
+        }
+    })
+}
+
+function setStartTime(videoId, userName) {
+    $.post("ajax/getProgress.php", { videoId: videoId, userName: userName}, function(data) {
+        if(isNaN(data)) { // if error then it is not a number else progress(int)
+            alert(data);
+            returnl
+        }
+        $("video").on("canplay", function() {
+            this.currentTime = data;
+            $("video").off("canplay");
+        })
+    })
 }
